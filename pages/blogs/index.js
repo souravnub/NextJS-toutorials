a98b18efc5eb677432e11d4012c7c3ab8c25b8c4
Sourav <s6hr6h4u3@gmail.com>
2022-12-27 18:54:34 +0530

    used SSR in blogsPage

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,13 +1,58 @@
 import React from "react";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const BlogsPage = () => {
-    // this page has no defined pre rendering method... but nextjs will by-default pre-render each and every page with SSG until-n-unless a pre-rendering method is not manually supplied
+const BlogsPage = ({ blogs }) => {
     return (
         <div>
-            BlogsPage
-            <h1>blogs page hahahahha</h1>
+            <h1>blogs page</h1>
+            {blogs.map(
+                ({ author, title, img, desc, read_time, topics, slug }) => {
+                    return (
+                        <div className={styles.blog} key={title}>
+                            <Image width={200} height={150} src={img} />
+                            <h1>{title}</h1>
+                            <p>{desc}</p>
+                            <span>{read_time}</span>
+
+                            {topics.map((topic) => {
+                                return topic;
+                            })}
+
+                            <Image
+                                src={author.profile_img}
+                                width={50}
+                                height={50}
+                            />
+
+                            <Link
+                                href={{
+                                    pathname: "/blogs/" + slug,
+                                }}>
+                                read more...
+                            </Link>
+                        </div>
+                    );
+                }
+            )}
         </div>
     );
 };
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blogs`);
+    try {
+        const data = await res.json();
+
+        return {
+            props: data, // will be passed to the page component as props
+        };
+    } catch (error) {
+        return {
+            notFound: true,
+        };
+    }
+}
 
 export default BlogsPage;
