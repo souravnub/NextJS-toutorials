309910520315c1b5701890de7e9cb9d17f522fb4
Sourav <s6hr6h4u3@gmail.com>
2022-12-27 18:58:05 +0530

    used SSR in SingleBlog page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,30 +1,25 @@
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const SingleBlog = () => {
-    // will have only <>loading...</> in its page source as it was by default rendered by nextjs with SSG ... but the content that is hydrated afterwards with client-side-javascript will not be shown in the page source ... as nextjs was not knowing that the page will be having it's content hydrated with client-side-js ... using a pre-rendering method for fetching data and hydration can resolve the issue
-    const [blog, setBlog] = useState(null);
-    const router = useRouter();
-
-    const getBlogs = async () => {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/blogs/${router.query.slug}`
-        );
-        const jsonres = await res.json();
-        if (jsonres.success) {
-            setBlog(jsonres.blog);
-        }
-    };
-
-    useEffect(() => {
-        getBlogs();
-    }, []);
-
-    if (!blog) {
-        return <>loading...</>;
-    }
-
+const SingleBlog = ({ blog: fetchedBlog }) => {
+    const [blog, setBlog] = useState(fetchedBlog);
     return <div>{JSON.stringify(blog)}</div>;
 };
+
+export async function getServerSideProps(context) {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/blogs/${context.query.slug}`
+    );
+    try {
+        const data = await res.json();
+
+        return {
+            props: data, // will be passed to the page component as props
+        };
+    } catch (error) {
+        return {
+            notFound: true,
+        };
+    }
+}
 
 export default SingleBlog;
