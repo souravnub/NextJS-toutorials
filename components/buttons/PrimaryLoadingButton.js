0f9ff066205fadeaca71f27a66e1d2c787da1c96
Sourav <s6hr6h4u3@gmail.com>
2023-01-07 12:51:55 +0530

    made accessible animations : PrimaryLoadingButton

--- components/buttons/PrimaryLoadingButton.js
+++ components/buttons/PrimaryLoadingButton.js
@@ -1,11 +1,61 @@
-import React, { useEffect, useRef } from "react";
-import { MdErrorOutline } from "react-icons/md";
+import React, { useEffect, useRef, useState } from "react";
+import { BiErrorCircle } from "react-icons/bi";
 import CircularLoader from "../loaders/circular loader/CircularLoader";
 import styles from "./buttons.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
-const findEleWidth = (ele) => {
-    return ele.getBoundingClientRect().width;
+const svgVariants = {
+    start: {
+        strokeDashoffset: "48px",
+    },
+    end: {
+        strokeDashoffset: 0,
+        transition: {
+            duration: 0.5,
+        },
+    },
 };
+
+const successContainerVariants = {
+    start: {
+        opacity: 0,
+    },
+    end: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.1,
+        },
+    },
+    exit: {
+        opacity: 0,
+    },
+};
+const errorContainerVariants = {
+    start: {
+        opacity: 0,
+    },
+    end: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.1,
+        },
+    },
+    exit: {
+        opacity: 0,
+    },
+};
+
+const successTextEle = {
+    start: {
+        opacity: 0,
+    },
+    end: {
+        opacity: 1,
+    },
+};
+
 // for this component to work the success state should be undefined when the request is send to backend, then true or false after loading....therefore it depends a bit on the api
 const PrimaryLoadingButton = ({
     children,
@@ -18,76 +68,48 @@ const PrimaryLoadingButton = ({
     successMsgColor,
     loaderStrokeColor = "var(--clr-primary-100)",
     //in ms
-    MsgDuration = 2000,
+    MsgDuration = 3000,
     onClick,
 }) => {
     const btnRef = useRef(null);
     const successMsgRef = useRef(null);
     const errorMsgRef = useRef(null);
 
-    const childrenContainerRef = useRef(null);
-    const successMsgContainerRef = useRef(null);
-    const errorMsgContainerRef = useRef(null);
+    const [currentState, setCurrentState] = useState("normal");
 
     useEffect(() => {
-        //transitioning the success msg alphabets
-        const alphabets = Array.from(successMsgRef.current.children);
-
-        alphabets.forEach((alphabet, idx) => {
-            alphabet.style.transitionDelay =
-                (100 * (idx + 1)) / alphabets.length + "ms";
-        });
-
-        // transitioning the error msg alphabets
-        const error_alp = Array.from(errorMsgRef.current.children);
-        error_alp.forEach((alphabet, idx) => {
-            alphabet.style.transitionDelay =
-                (100 * (idx + 1)) / error_alp.length + "ms";
-        });
-
-        // finding suitable width of the button
-        const widthArr = [
-            findEleWidth(childrenContainerRef.current),
-            findEleWidth(successMsgContainerRef.current),
-            findEleWidth(errorMsgContainerRef.current),
-        ];
-
-        let largestWidth = widthArr[0];
-        widthArr.forEach((width) => {
-            if (width > largestWidth) {
-                largestWidth = width;
-            }
-        });
-
-        btnRef.current.style.minWidth = largestWidth + "px";
-    }, []);
-
-    useEffect(() => {
-        const btn = btnRef.current;
-
         const timeout = setTimeout(() => {
-            if (btn.classList.contains(styles.success_state)) {
-                btn.classList.remove(styles.success_state);
-            } else if (btn.classList.contains(styles.error_state)) {
-                btn.classList.remove(styles.error_state);
+            if (currentState === "success" || currentState === "error") {
+                setCurrentState("normal");
             }
         }, MsgDuration);
 
         return () => {
             clearTimeout(timeout);
         };
-    }, [isSuccess]);
+    }, [isSuccess, currentState]);
+
+    useEffect(() => {
+        if (isLoading) {
+            setCurrentState("loading");
+        }
+        if (isSuccess === true) {
+            setCurrentState("success");
+        }
+        if (isSuccess === false) {
+            setCurrentState("error");
+        }
+        if (!isLoading && isSuccess === undefined) {
+            setCurrentState("normal");
+        }
+    }, [isLoading, isSuccess]);
 
     return (
         <>
             <button
                 ref={btnRef}
                 onClick={onClick}
-                className={`${styles.primary_loading_btn} ${
-                    isLoading === true ? styles.loading_state : ""
-                } ${isSuccess === true ? styles.success_state : ""} ${
-                    isSuccess === false ? styles.error_state : ""
-                }`}
+                className={styles.primary_loading_btn}
                 disabled={isLoading}
                 style={inlineStyles}>
                 {/* children copy will provide button height and width */}
@@ -95,59 +117,105 @@ const PrimaryLoadingButton = ({
                     {children}
                 </div>
 
-                <div
-                    className={styles.children_container}
-                    ref={childrenContainerRef}>
-                    {children}
-                </div>
+                <AnimatePresence>
+                    {currentState === "normal" && (
+                        <motion.div
+                            className={styles.children_container}
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                opacity: 1,
+                                y: 0,
+                                transition: { delay: 0.5 },
+                            }}
+                            exit={{ opacity: 0, y: "100%" }}>
+                            {children}
+                        </motion.div>
+                    )}
+                </AnimatePresence>
 
-                <div className={styles.loader_container}>
-                    <CircularLoader
-                        thickness="4px"
-                        strokeColor={loaderStrokeColor}
-                    />
-                </div>
+                <AnimatePresence>
+                    {currentState === "loading" && (
+                        <motion.div
+                            className={styles.loader_container}
+                            initial={{ y: "-100%", opacity: 0 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ y: "100%", opacity: 0 }}>
+                            <CircularLoader
+                                thickness="4px"
+                                strokeColor={loaderStrokeColor}
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
 
-                <div
-                    style={{ "--color": successMsgColor }}
-                    className={styles.success_msg_container}
-                    ref={successMsgContainerRef}>
-                    <svg
-                        className={styles.checkmark}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 52 52">
-                        <path
-                            className={styles.checkmark_check}
-                            fill="none"
-                            d="M14.1 27.2l7.1 7.2 16.7-16.8"
-                        />
-                    </svg>
-                    <div className={styles.success_msg} ref={successMsgRef}>
-                        {[...successMsg].map((alp, idx) => {
-                            if (alp === " ") {
-                                return <span key={idx}>&nbsp;</span>;
-                            } else {
-                                return <span key={idx}>{alp}</span>;
-                            }
-                        })}
-                    </div>
-                </div>
+                <AnimatePresence>
+                    {currentState === "success" && (
+                        <motion.div
+                            variants={successContainerVariants}
+                            initial="start"
+                            animate="end"
+                            exit="exit"
+                            style={{ "--color": successMsgColor }}
+                            className={styles.success_msg_container}>
+                            <svg
+                                className={styles.checkmark}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 52 52">
+                                <motion.path
+                                    variants={svgVariants}
+                                    className={styles.checkmark_check}
+                                    fill="none"
+                                    d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                                />
+                            </svg>
+                            <div
+                                className={styles.success_msg}
+                                ref={successMsgRef}>
+                                {[...successMsg].map((alp, idx) => {
+                                    return (
+                                        <motion.span
+                                            variants={successTextEle}
+                                            key={idx}>
+                                            {alp === " " ? <>&nbsp;</> : alp}
+                                        </motion.span>
+                                    );
+                                })}
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
 
-                <div
-                    style={{ "--color": errorMsgColor }}
-                    className={styles.error_msg_container}
-                    ref={errorMsgContainerRef}>
-                    <MdErrorOutline />
-                    <div ref={errorMsgRef} className={styles.error_msg}>
-                        {[...errorMsg].map((alp, idx) => {
-                            if (alp === " ") {
-                                return <span key={idx}>&nbsp;</span>;
-                            } else {
-                                return <span key={idx}>{alp}</span>;
-                            }
-                        })}
-                    </div>
-                </div>
+                <AnimatePresence>
+                    {currentState === "error" && (
+                        <motion.div
+                            style={{ "--color": errorMsgColor }}
+                            variants={errorContainerVariants}
+                            initial="start"
+                            animate="end"
+                            exit="exit"
+                            className={styles.error_msg_container}>
+                            <motion.div
+                                className={styles.svg_container}
+                                variants={successTextEle}>
+                                <BiErrorCircle
+                                    size="20px"
+                                    style={{ "--color": successMsgColor }}
+                                />
+                            </motion.div>
+                            <div ref={errorMsgRef} className={styles.error_msg}>
+                                {[...errorMsg].map((alp, idx) => {
+                                    return (
+                                        <motion.span
+                                            variants={successTextEle}
+                                            key={idx}>
+                                            {alp === " " ? <>&nbsp;</> : alp}
+                                        </motion.span>
+                                    );
+                                })}
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </button>
         </>
     );
