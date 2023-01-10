30eb2fcfc89fc329ac764e255d5ea1afdd029fd5
Sourav <s6hr6h4u3@gmail.com>
2023-01-10 20:07:08 +0530

    blogs can be fetched with params

--- utils/fetchBlogs.js
+++ utils/fetchBlogs.js
@@ -1,12 +1,20 @@
-export default async function fetchBlogs({ type, following }) {
+function genURL(url, params) {
+    if (url.includes("?")) {
+        return url + "&" + new URLSearchParams(params);
+    }
+
+    return url + "?" + new URLSearchParams(params);
+}
+
+export default async function fetchBlogs({ type, following, params }) {
     let serach_string;
 
     if (type === "following") {
-        serach_string = `/api/blogs?type=${type}&following=${following}`;
+        serach_string = genURL("/api/blogs", { type, following, ...params });
     } else if (type) {
-        serach_string = `/api/blogs?type=${type}`;
+        serach_string = genURL("/api/blogs", { type, ...params });
     } else {
-        serach_string = `/api/blogs`;
+        serach_string = genURL("/api/blogs", { ...params });
     }
 
     const res = await fetch(serach_string);
